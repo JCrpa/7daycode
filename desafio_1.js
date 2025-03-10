719d7e6a5b7410af8daf0062feff619aff91e7cb
Joao Costa <joaocosta.info@gmail.com>
2025-03-09 22:21:24 -0300

    reestruturando_html_css

--- desafio_1.js
+++ desafio_1.js
@@ -1,23 +1,37 @@
 let numeroUm = 1;
 let stringUm = '1';
 
-if (1 === '1' ) {
-    document.getElementById("resultado1").textContent = "As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes";
+if (numeroUm == stringUm) {
+    document.getElementById("resultado1").textContent ="As variáveis 1 e '1' tem o mesmo valor, mas tipos diferentes";
     } else {
-    document.getElementById("resultado1").textContent ="As variáveis numeroUm e stringUm não tem o mesmo valor";
+    document.getElementById("resultado1").textContent ="As variáveis 1 e '1' não tem o mesmo valor";
     }
-
+if (numeroUm === stringUm) {
+    document.getElementById("resultado4").textContent ="As variáveis 1 e '1' tem o mesmo valor, mas tipos diferentes";
+    } else {
+    document.getElementById("resultado4").textContent ="As variáveis 1 e '1' não tem o mesmo valor";
+    }
 let numeroDez = 10;
 let stringDez = '10';
-if (10 === '10') {
-    document.getElementById("resultado2").textContent ="As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes";
+if (numeroDez == stringDez) {
+    document.getElementById("resultado2").textContent ="As variáveis 10 e '10' tem o mesmo valor, mas tipos diferentes";
     } else {
-    document.getElementById("resultado2").textContent ="As variáveis numeroDez e stringDez não tem o mesmo valor";
+    document.getElementById("resultado2").textContent ="As variáveis 10 e '10' não tem o mesmo valor";
+    }
+if (numeroDez === stringDez) {
+    document.getElementById("resultado5").textContent ="As variáveis 10 e '10' tem o mesmo valor, mas tipos diferentes";
+    } else {
+    document.getElementById("resultado5").textContent ="As variáveis 10 e '10' não tem o mesmo valor";
     }
 let numeroTrinta = 30;
 let stringTrinta = '30';
-if (30 === '30' ) {
-    document.getElementById("resultado3").textContent ="As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo";
+if (numeroTrinta == stringTrinta) {
+    document.getElementById("resultado3").textContent ="As variáveis 30 e '30' tem o mesmo valor e mesmo tipo";
     } else {
-    document.getElementById("resultado3").textContent ="As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo";
+    document.getElementById("resultado3").textContent ="As variáveis 30 e '30' não tem o mesmo tipo";
+    }
+if (numeroTrinta === stringTrinta) {
+    document.getElementById("resultado6").textContent ="As variáveis 30 e '30' tem o mesmo valor e mesmo tipo";
+    } else {
+    document.getElementById("resultado6").textContent ="As variáveis 30 e '30' não tem o mesmo tipo";
     }
