85d2156b01140459685ba207501ed1602e23ab61
Joao Costa <joaocosta.info@gmail.com>
2025-03-09 21:00:57 -0300

    inicial

---
+++ desafio_1.js
@@ -0,0 +1,23 @@
+let numeroUm = 1;
+let stringUm = '1';
+
+if (1 === '1' ) {
+    document.getElementById("resultado1").textContent = "As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes";
+    } else {
+    document.getElementById("resultado1").textContent ="As variáveis numeroUm e stringUm não tem o mesmo valor";
+    }
+
+let numeroDez = 10;
+let stringDez = '10';
+if (10 === '10') {
+    document.getElementById("resultado2").textContent ="As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes";
+    } else {
+    document.getElementById("resultado2").textContent ="As variáveis numeroDez e stringDez não tem o mesmo valor";
+    }
+let numeroTrinta = 30;
+let stringTrinta = '30';
+if (30 === '30' ) {
+    document.getElementById("resultado3").textContent ="As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo";
+    } else {
+    document.getElementById("resultado3").textContent ="As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo";
+    }
